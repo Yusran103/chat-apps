641a8d0eee66fd519ceecc2d9539c0634b866a9d
Yusran Ihza Mahendra <edo.hunt312@gmail.com>
2023-09-24 23:36:38 +0700

    feat: moment js, beautify

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jquery-rails'
 gem 'simple_form'
 # Use Redis adapter to run Action Cable in production
 gem 'redis', '~> 4.0'
+gem 'momentjs-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -16,7 +16,7 @@ $(function() {
             var content = messageTemplate.children().clone(true, true);
             content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
             content.find('[data-role="message-text"]').text(data.message);
-            content.find('[data-role="message-date"]').text(data.updated_at);
+            content.find('[data-role="message-date"]').text(moment(data.updated_at).format('LLLL'));
             $element.append(content);
             $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
           }
